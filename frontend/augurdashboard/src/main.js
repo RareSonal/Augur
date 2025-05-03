9ea80a7073013846a6d5131bad0a52fc53d27bd4
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-03 18:26:39 +0530

    Delete frontend/augurdashboard directory

--- frontend/augurdashboard/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';  // Import Element Plus styles
-
-const app = createApp(App);
-app.use(ElementPlus);  // Use Element Plus globally
-app.mount('#app');
